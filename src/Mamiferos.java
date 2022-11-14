b5dc0caa94e0c0af74400ddaf2cc8791a7b46ce7
POO <melani.molina@epn.edu.ec>
2022-11-14 10:53:33 -0500

    cambios

--- src/Mamiferos.java
+++ src/Mamiferos.java
@@ -5,4 +5,20 @@ public class Mamiferos {
         nombre_ani = "Jirafa";
         alimenta = "Hojas";
     }
+
+    public String getNombre_ani() {
+        return nombre_ani;
+    }
+
+    public void setNombre_ani(String nombre_ani) {
+        this.nombre_ani = nombre_ani;
+    }
+
+    public String getAlimenta() {
+        return alimenta;
+    }
+
+    public void setAlimenta(String alimenta) {
+        this.alimenta = alimenta;
+    }
 }
